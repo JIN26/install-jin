d10c05b18166cb3084d52cfcb3a1a1eafad8b6be
JIN26 <Angelmantilla26@gmail.com>
2021-03-16 15:10:52 -0500

    update install.pkg

--- index.sh
+++ index.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
+
 PWDL=$PWD
+#sudo apt install wget && curl proot tar 
+dlink="raw.githubusercontent.com/JIN26/install/main"
+folder=.Jin
+
+#Jin
+    if [ -d "~/$folder" ];then
+        echo "skipping dowloading"
+    else
+        echo "Download package. this may take a while base on your internet speed."
+        #repos ={[0]=Banner [1]=Git.sh}
+        #for repo in $repos; do
+        #    echo -e " ${color["Azul"]}[${color["Blanco1"]}$i${color["Azul"]}]${color["Blanco1"]} ${repo[$((++i))]}"
+        #done
+        #case
+        mkdir -p ~/$folder
+    fi
+#lib
+    sudo mkdir -p /usr/local/lib/$folder
+    sudo wget "https://${dlink}/lib/network.lib?raw=true" -O /usr/local/lib/$folder/network.lib  
 
 #package.sh
-    chmod +x src/package/install.pkg
-    . src/package/install.pkg
+    #curl https://${dlink}/package/install.pkg | bash
+
 #logo
     if [ -f ~/.Jin/logo ];then
-        echo
+        echo "neofetch"
     else
         sudo ln -s $PWDL/img/logo.sh logo
         chmod +x logo
@@ -15,6 +35,6 @@ PWDL=$PWD
     fi
 
 #Servidor local $ipp
-    cd $PWDL
-    chmod +x src/cmd/vnc.sh
-    . src/cmd/vnc.sh &
+    #cd $PWDL
+    #chmod +x src/cmd/vnc.sh
+    #. src/cmd/vnc.sh &
