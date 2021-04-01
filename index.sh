db57a4857fdd33630462cac6fcba4e6a7911a053
JIN26 <Angelmantilla26@gmail.com>
2021-03-31 20:38:19 -0500

    add automatic plugins install

--- index.sh
+++ index.sh
@@ -28,8 +28,11 @@ mkdir -p ~/$folder
         #bin jin
             if [ ! -f ~/usr/local/bin/jin ];then
                 sudo wget "https://${dlink}/bin/raw.bin?raw=true" -O /usr/local/bin/jin  
-                sudo chmod +x /usr/local/bin/jin
-            fi
+		sudo chmod +x /usr/local/bin/jin
+	    fi
+	#Plugins 
+	    if [ ! -f ~/.config/nvim/init.vim ];then
+		sudo wget "https://${dlink}/plugins/init.vim?raw=true" -O /.config/nvim/init.vim  
     fi
 
 #alias de linux
