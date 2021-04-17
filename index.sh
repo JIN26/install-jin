4a28d7bf3f85018b1345b350328a6ee8b5e753c3
JIN26 <Angelmantilla26@gmail.com>
2021-04-17 14:53:25 -0500

    add banner bin

--- index.sh
+++ index.sh
@@ -15,6 +15,10 @@ sudo wget "https://${dlink}/lib/network.lib?raw=true" -O /usr/local/lib/$folder/
 sudo rm /usr/local/lib/$folder/aliases.lib
 sudo wget "https://${dlink}/lib/alias.lib?raw=true" -O /usr/local/lib/$folder/aliases.lib  
 
+#lib autor
+sudo rm /usr/local/lib/$folder/autor.lib
+sudo wget "https://${dlink}/lib/autor.lib?raw=true" -O /usr/local/lib/$folder/autor.lib  
+
 #bin logo
 sudo rm /usr/local/bin/logo
 sudo wget "https://raw.githubusercontent.com/JIN26/Banner/master/src/bin/Banner.bin?raw=true" -O /usr/local/bin/logo
@@ -29,7 +33,40 @@ sudo chmod +x /usr/local/bin/jin
 sudo rm ~/.config/nvim/init.vim
 sudo wget "https://${dlink}/plugins/init.vim?raw=true" -O ~/.config/nvim/init.vim  
 
-#alias de linux
-echo "#User default
-source /usr/local/lib/$folder/aliases.lib
-logo">~/.bash_aliases
+sleep 0.5
+#Rescribir el banner dependiendo del sistema
+case "$OSTYPE" in
+  linux-androideabi)
+    if [ -e $PREFIX/etc/motd ]; then
+      rm $PREFIX/etc/motd
+    fi
+    if [ -e $PREFIX/etc/bash.bashrc ]; then
+      echo "
+      if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
+        command_not_found_handle() {
+          /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+        }
+      fi
+
+      PS1='Jin$ '
+
+      #User default
+      source /usr/local/lib/$folder/aliases.lib
+      logo">$PREFIX/etc/bash.bashrc
+    fi
+    ;;
+  linux-gnu)
+    #alias de linux
+    echo "#User default
+    source /usr/local/lib/$folder/aliases.lib
+    logo">~/.bash_aliases
+    #echo "# Alias definitions.
+    # You may want to put all your additions into a separate file like
+    # ~/.bash_aliases, instead of adding them here directly.
+    # See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
+    #if [ -f ~/.bash_aliases ]; then
+    #  . ~/.bash_aliases
+    #fi">>~/.bashrc
+  ;;
+esac
