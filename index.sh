5491865c19c273722a47717ddd51395c2f0f20b8
JIN26 <Angelmantilla26@gmail.com>
2021-11-28 23:57:08 -0500

    index con condicionales

--- index.sh
+++ index.sh
@@ -1,42 +1,73 @@
 #!/bin/bash
 
-#Jin
 echo -e "\n\tDownload package. this may take a while base on your internet speed.\n"
-dlink="raw.githubusercontent.com/JIN26/install/main"
+
 folder=.Jin
-mkdir -p ~/$folder
+
+#
+#			LET'S TO START
+#
+
+#Cleaning up
+
+	case "$OSTYPE" in
+  		linux-androideabi)
+			dlink="raw.githubusercontent.com/JIN26/install/termux"
+			usr="$PREFIX"
+			;;
+		linux-gnu)
+			dlink="raw.githubusercontent.com/JIN26/install/main"
+                        usr="/usr/local"
+			;;
+	esac
+
+#.Jin	
+	if [ -e ~/$folder ]; then                      
+                rm ~/$folder                             
+        fi
+
+	mkdir -p ~/$folder
+        
+	if [ -e ${usr}/var/lib/$folder ]; then                      
+                rm ${usr}/var/lib/$folder                             
+        fi
+
+	mkdir -p /${usr}/lib/$folder 
 
 #lib network
-mkdir -p /usr/local/lib/$folder
-rm /usr/local/lib/$folder/network.lib
-wget "https://${dlink}/lib/network.lib?raw=true" -O /usr/local/lib/$folder/network.lib 
-chmod +xr /usr/local/lib/$folder/network.lib
+	if [ -e ${usr}/var/lib/$folder/network.lib ]; then                      
+                rm ${usr}/var/lib/$folder/network.lib                             
+        fi
+              
+	wget "https://${dlink}/lib/network.lib?raw=true" -O ${usr}/lib/$folder/network.lib 
+	chmod +xr ${usr}/lib/$folder/network.lib
 
 #lib aliases
-rm /usr/local/lib/$folder/aliases.lib
-wget "https://${dlink}/lib/alias.lib?raw=true" -O /usr/local/lib/$folder/aliases.lib  
-chmod +xr /usr/local/lib/$folder/aliases.lib
+	rm /usr/local/lib/$folder/aliases.lib
+	wget "https://${dlink}/lib/alias.lib?raw=true" -O /usr/local/lib/$folder/aliases.lib  
+	chmod +xr /usr/local/lib/$folder/aliases.lib
 
 #lib autor
-rm /usr/local/lib/$folder/autor.lib
-wget "https://${dlink}/lib/autor.lib?raw=true" -O /usr/local/lib/$folder/autor.lib  
-chmod +xr /usr/local/lib/$folder/autor.lib
+	rm /usr/local/lib/$folder/autor.lib
+	wget "https://${dlink}/lib/autor.lib?raw=true" -O /usr/local/lib/$folder/autor.lib  
+	chmod +xr /usr/local/lib/$folder/autor.lib
 
 #bin logo
-rm /usr/local/bin/logo
-wget "https://raw.githubusercontent.com/JIN26/Banner/master/src/bin/Banner.bin?raw=true" -O /usr/local/bin/logo
-chmod +xr /usr/local/bin/logo
+	rm /usr/local/bin/logo
+	wget "https://raw.githubusercontent.com/JIN26/Banner/master/src/bin/Banner.bin?raw=true" -O /usr/local/bin/logo
+	chmod +xr /usr/local/bin/logo
 
 #bin jin
-rm /usr/local/bin/jin
-wget "https://${dlink}/bin/raw.bin?raw=true" -O /usr/local/bin/jin  
-chmod +x /usr/local/bin/jin
+	rm /usr/local/bin/jin
+	wget "https://${dlink}/bin/raw.bin?raw=true" -O /usr/local/bin/jin  
+	chmod +x /usr/local/bin/jin
 
 #Plugins neovim
-rm ~/.config/nvim/init.vim
-wget "https://${dlink}/plugins/init.vim?raw=true" -O ~/.config/nvim/init.vim  
+	rm ~/.config/nvim/init.vim
+	wget "https://${dlink}/plugins/init.vim?raw=true" -O ~/.config/nvim/init.vim  
 
-sleep 0.5
+	sleep 0.5
+
 #Rescribir el banner dependiendo del sistema
 case "$OSTYPE" in
   linux-androideabi)
