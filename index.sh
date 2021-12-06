d823b17cbf28a7cc2da1e32e468af0fbd9c18007
JIN26 <Angelmantilla26@gmail.com>
2021-12-05 22:26:07 -0500

    index update

--- index.sh
+++ index.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+!/bin/bash
 
 echo -e "\n\tDownload package. this may take a while base on your internet speed.\n"
 
-folder=.Jin
+#.Jin	
+    folder=.Jin
+    if [ -e ~/$folder ]; then                      rm -r ~/$folder                             
+	rm -r ~/$folder 
+    fi
+    mkdir -p ~/$folder
 
 #
 #			LET'S TO START
@@ -10,67 +15,94 @@ folder=.Jin
 
 #Cleaning up
 
-	case "$OSTYPE" in
-  		linux-androideabi)
-			dlink="raw.githubusercontent.com/JIN26/install/termux"
-			usr="$PREFIX"
-			;;
-		linux-gnu)
-			dlink="raw.githubusercontent.com/JIN26/install/main"
-                        usr="/usr/local"
-			;;
-	esac
+    case "$OSTYPE" in
+	linux-androideabi)
+	    dlink="raw.githubusercontent.com/JIN26/install/termux"
+	    usr="$PREFIX" 
+	;;
+	linux-gnu)
+	    dlink="raw.githubusercontent.com/JIN26/install/main"
+            usr="/usr/local"			
+	;;
+    esac
 
-#.Jin	
-	if [ -e ~/$folder ]; then                      
-                rm ~/$folder                             
-        fi
+function ctrl_c() {
+    printf "        $R [W] You will exit $W \n"
+    printf "$Y [!]$W Everything will be restored \n\n"
+    sleep 1
+    
+    #.Jin	
+    if [ -e ${usr}/lib/$folder ]; then                      
+	rm -r ${usr}/lib/$folder                             
+    fi
+    
+    #lib network
+    if [ -e ${usr}/lib/$folder/network.lib ]; then                      
+	rm ${usr}/lib/$folder/network.lib                             
+    fi
 
-	mkdir -p ~/$folder
-        
-	if [ -e ${usr}/var/lib/$folder ]; then                      
-                rm ${usr}/var/lib/$folder                             
-        fi
+    #lib aliases
+    if [ -e ${usr}/lib/$folder/aliases.lib ]; then                      
+	rm ${usr}/lib/$folder/aliases.lib 
+    fi
 
-	mkdir -p /${usr}/lib/$folder 
+    #lib autor
+    if [ -e ${usr}/lib/$folder/autor.lib ]; then                      
+	rm ${usr}/lib/$folder/autor.lib
+    fi
 
-#lib network
-	if [ -e ${usr}/var/lib/$folder/network.lib ]; then                      
-                rm ${usr}/var/lib/$folder/network.lib                             
-        fi
-              
-	wget "https://${dlink}/lib/network.lib?raw=true" -O ${usr}/lib/$folder/network.lib 
-	chmod +xr ${usr}/lib/$folder/network.lib
-
-#lib aliases
-	rm /usr/local/lib/$folder/aliases.lib
-	wget "https://${dlink}/lib/alias.lib?raw=true" -O /usr/local/lib/$folder/aliases.lib  
-	chmod +xr /usr/local/lib/$folder/aliases.lib
-
-#lib autor
-	rm /usr/local/lib/$folder/autor.lib
-	wget "https://${dlink}/lib/autor.lib?raw=true" -O /usr/local/lib/$folder/autor.lib  
-	chmod +xr /usr/local/lib/$folder/autor.lib
-
-#bin logo
-	rm /usr/local/bin/logo
-	wget "https://raw.githubusercontent.com/JIN26/Banner/master/src/bin/Banner.bin?raw=true" -O /usr/local/bin/logo
-	chmod +xr /usr/local/bin/logo
-
-#bin jin
-	rm /usr/local/bin/jin
-	wget "https://${dlink}/bin/raw.bin?raw=true" -O /usr/local/bin/jin  
-	chmod +x /usr/local/bin/jin
-
-#Plugins neovim
+    #bin logo
+    if [ -e ${usr}/bin/logo ]; then                      
+	rm ${usr}/bin/logo	
+    fi
+    
+    #bin jin
+    if [ -e ${usr}/bin/jin ]; then                      
+	rm ${usr}/bin/jin
+    fi 
+    
+    #Plugins neovim
+    if [ -e ~/.config/nvim/init.vim ]; then
 	rm ~/.config/nvim/init.vim
-	wget "https://${dlink}/plugins/init.vim?raw=true" -O ~/.config/nvim/init.vim  
-
-	sleep 0.5
+    fi
+    
+    echo $(clear)
+    echo $(exit)
+}
+              
+sleep 0.5
 
 #Rescribir el banner dependiendo del sistema
+
 case "$OSTYPE" in
   linux-androideabi)
+    
+    #.Jin
+	mkdir -p /${usr}/lib/$folder 
+    
+    #netword.lib
+	wget "https://${dlink}/lib/network.lib?raw=true" -O ${usr}/lib/$folder/network.lib 
+	chmod +xr ${usr}/lib/$folder/network.lib
+    
+    #aliases.lib
+	wget "https://${dlink}/lib/alias.lib?raw=true" -O ${usr}/lib/$folder/aliases.lib  
+	chmod +xr ${usr}/lib/$folder/aliases.lib
+    
+    #autor.lib
+	wget "https://${dlink}/lib/autor.lib?raw=true" -O ${usr}/lib/$folder/autor.lib  
+	chmod +xr ${usr}/lib/$folder/autor.lib
+    
+    #bin logo
+	wget "https://raw.githubusercontent.com/JIN26/Banner/master/src/bin/Banner.bin?raw=true" -O ${usr}/bin/logo
+	chmod +xr ${usr}/bin/logo
+    
+    #bin jin
+	wget "https://${dlink}/bin/raw.bin?raw=true" -O ${usr}/bin/jin  
+	chmod +x ${usr}/bin/jin
+
+    #Plugins neovim
+	wget "https://${dlink}/plugins/init.vim?raw=true" -O ~/.config/nvim/init.vim  
+
     if [ -e $PREFIX/etc/motd ]; then
       rm $PREFIX/etc/motd
     fi
@@ -85,15 +117,18 @@ case "$OSTYPE" in
       PS1='Jin$ '
 
       #User default
-      source /usr/local/lib/$folder/aliases.lib
+      source ${usr}/lib/$folder/aliases.lib
       logo">$PREFIX/etc/bash.bashrc
     fi
     ;;
-  linux-gnu)
+    linux-gnu)
+    
     #alias de linux
+    
     echo "#User default
-    source /usr/local/lib/$folder/aliases.lib
+    source ${usr}/lib/$folder/aliases.lib
     logo">~/.bash_aliases
+    
     #echo "# Alias definitions.
     # You may want to put all your additions into a separate file like
     # ~/.bash_aliases, instead of adding them here directly.
@@ -102,5 +137,6 @@ case "$OSTYPE" in
     #if [ -f ~/.bash_aliases ]; then
     #  . ~/.bash_aliases
     #fi">>~/.bashrc
-  ;;
+    
+    ;;
 esac
